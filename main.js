3d5c1c0598b0f47b2d6f49d20dc720f935966188
Vlasya <64101971+Vlasya@users.noreply.github.com>
2020-09-06 22:05:38 +0300

    done

--- main.js
+++ main.js
@@ -7,15 +7,30 @@
 // Если аргументы a и b не переданы, они равны по умолчанию 2 и 3 соответсвенно.
 // Если аргумент c передан и он является функцией, то он выполняется после вызова функции sum.
 // Функция f должны возвращать результат функции аргумента c, если он есть, либо результат функции sum.
-
+var a;
+var b;
+var c;
 function f(a=2,b=3,c){
-	function sum(a,b){
-		return a+b;
+	for (let i=0;i<arguments.length;i++){
+		var argFunc=false;
+		if(typeof arguments[2]==='function'){
+			sum(a,b)
+			argFunc=arguments[2];
+			argFunc()
+		}
+		
 	}
-	// console.log(a);
-	// console.log(b);
-	// console.log(c);
-
+	if (argFunc){
+		return argFunc()
+	}else{
+		return sum(a,b)
+	}	
 }
-let result =f();
-console.log(result);
+
+
+function sum (a, b) { 
+	return a + b; 
+} 
+
+console.log('Функция f(): ', f(3,5,function(){return 'Функция из третьего ( C ) агрумента'}));
+
